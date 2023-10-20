62d6ee991ba6a90a1ecb134a67b084d8b46fe677
Himendra <himsibaba@gmail.com>
2023-10-20 14:59:59 -0400

    Added auto data tested, plus other things

--- CO2V10inverse.py
+++ CO2V10inverse.py
@@ -32,7 +32,7 @@ dde.config.real.set_float64()
 class CO2Sim():
 
 
-    def __init__(self,name):
+    def __init__(self,name,filelocation):
 
         ## system variables
         self.R = 0.01
@@ -41,11 +41,13 @@ class CO2Sim():
         self.angularVelocity = 1
         self.name = name
         self.a_n_list = []
-        self.an = self.GenerateAn(10)
+        self.an = self.GenerateAn(50)
         self.CrossParams = [1.491095, 6.56460753e-1, 8754.40717e3, 25]
         self.Temperature = 100
         self.Pressure = 10
         self.TimeScale = 10800
+        self.modelName = ""
+        self.LoadModel = False
         #eta will switch to numpy array later on
         #eta will be in the form of mx+b, where x is time. Guess m and b. m = 0.6,
         self.solubilityModel = self.importSolubilityModel("./SolubilityPS.xlsx")
@@ -56,24 +58,31 @@ class CO2Sim():
 
         #self.etaexp = dde.Variable(0.1, dtype=tf.float64)
         #self.eta = 10**self.etaexp
-        self.Dexp = dde.Variable(-12, dtype=tf.float64)
+
+        # this is variable D, which is limited to 10^-10 to 10 ^-8
+        self.Dexp = dde.Variable(-9, dtype=tf.float64)
+
+
+        #self.tauFactor = dde.Variable(1, dtype=tf.float64)
         #self.b = dde.Variable(0.1, dtype=tf.float64)
 
         self.etaInit = 3000
         self.r = 4
         self.t = 1
         self.calcTorque = 167.55
+        self.torqueScale = 1
+        #self.exptorqueScale = 1
         #be able to call system variables:
 
 
 
-        self.location = "./NewTrainingData.csv"
+        self.location = filelocation
         self.xData, self.torqueData = self.importData(self.location)
 
         #define eta. Should be a NP array
 
         #### setting up domain
-        self.geom = dde.geometry.Interval(self.Rmin, self.R)
+        self.geom = dde.geometry.Interval(self.Rmin, 1)
         self.timedomain = dde.geometry.TimeDomain(0.001, 1)
         self.geomtime = dde.geometry.GeometryXTime(self.geom, self.timedomain)
         self.bc = self.generateBC()
@@ -81,47 +90,62 @@ class CO2Sim():
         ### setting up model
         self.data = dde.data.TimePDE(
             self.geomtime,
-            self.rheometerPlateCO2Diff,
+            self.rheometerPlateCO2Diff2,
             self.bc,
             num_domain=800,
             num_boundary=1000,
-            #num_initial = 100,
+            num_initial = 100,
             #solution = self.initialCondition
             #auxiliary_var_function=ex_func2,
         )
 
         self.external_trainable_variables = [self.Dexp]
         self.variable = dde.callbacks.VariableValue(
-            self.external_trainable_variables, period=200, filename="variables1.dat"
+            self.external_trainable_variables, period=1000, filename="./variableTrained/"+self.name+"variables1.dat"
         )
 
 
         self.net = dde.nn.FNN([2] + [80] * 5 + [1], "tanh", "Glorot uniform")
-        self.net.apply_output_transform(self.transformFxn)
+        self.net.apply_output_transform(self.transformFxn2)
         self.model = dde.Model(self.data, self.net)
-
+        #self.loadModel()
 
 
 ###############################################################################
 ##backend/ generation of system constants/math stuff
-
+    #import data from excel. Returns a model that takes in pressure and temperature. This will be run once to get max solubility for a run.
+    def importSolubilityModel(self, locationOfData):
+        DataForPS = pd.read_excel(locationOfData, header = [0])
+        x= DataForPS["Pressure (MPa)"]
+        y=DataForPS["Temperature (K)"]
+        z=DataForPS["Solubility"]
+        Model = SmoothBivariateSpline(x,y,z,kx=2,ky=2, s = 0.0001,eps=0.02)
+        return Model
 
     def importData(self, location):
         Data = pd.read_csv(location)
-        torque = [(Data["tau"])]
-        #print(torque)
-        time = [(Data["t"])]
+        self.torqueScale = Data["tau"].max()
+        self.TimeScale = Data["t1"].max()
+        #print("torquescale", self.torqueScale)
+        Data["tau_scale"] = Data["tau"]/self.torqueScale
+        #Data["time_scale"] = Data["time"]/self.TimeScale
+
+        torque = [(Data["tau_scale"].to_numpy())]
+        print(torque)
+        time = [(Data["t"].to_numpy())]
         #print(time)
 
-        r = [(Data["r"])]
+        r = [(Data["r"].to_numpy())]
         self.Temperature = Data["Temperature"].mean()
         self.Pressure = Data["Pressure"].mean()
         self.etaInit = Data["eta"].iloc[0]
         #print(r)
         #R, T = np.meshgrid(r, time)
         r = np.reshape(r, (-1, 1))
+
         t = np.reshape(time, (-1, 1))
         Torque =  np.reshape(torque, (-1, 1))
+        print(Torque)
 
         return np.hstack((r,t)), Torque
 
@@ -136,8 +160,11 @@ class CO2Sim():
 
     #defines the concentration of co2 analytically
     def concentrationSln(self,x):
-        r = tf.reshape(x[:,0:1],[-1,-1,1])
-        t = tf.reshape(x[:,1:2],[-1,-1,1])
+        #r = tf.expand_dims(x[:,0:1],2)
+        #t = tf.expand_dims(x[:,1:2],2)
+
+
+
         #print("r",r)
         #print("yoho", x[:,0:1])
         R = self.R
@@ -145,13 +172,36 @@ class CO2Sim():
         a_n = self.a_n_list
         #print("rda",R,D,a_n)
 
-        print(a_n)
+        #print(a_n)
         #bessel_result = tf.math.special.bessel_j0(a_n * r / R)/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**(self.Dexp))*t/R**2)
-        result_sum = 1-2*dde.backend(tf.math.special.bessel_j0(a_n * r / R)/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**(self.Dexp))*t/R**2), axis=2)
+        #result_sum = 1-2*dde.backend(tf.math.special.bessel_j0(a_n * r / R)/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**(self.Dexp))*t/R**2), axis=2)
 
-        print(2*tf.reduce_sum(tf.math.special.bessel_j0(a_n * r / R)/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**(self.Dexp))*t/R**2), axis=2))
 
-        return 1-2*tf.reduce_sum(tf.math.special.bessel_j0(a_n*r/R)/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**(self.Dexp))*t/R**2), axis=2)
+
+        #sum1 = 1-2*tf.reduce_sum(tf.divide(2*tf.math.special.bessel_j0( tf.reduce_sum(tf.multiply(a_n,x[:,0:1])))*tf.exp( tf.reduce_sum(tf.multiply(a_n**2,-1*x[:,1:2]))*(10**(self.diffusionModel()))/R**2), (tf.math.special.bessel_j1(a_n)*a_n)))
+
+        #sum1 =  1-2*tf.math.special.bessel_j0( tf.reduce_sum(tf.multiply(a_n,x[:,0:1])))/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp( tf.reduce_sum(tf.multiply(a_n**2,-1*x[:,1:2]))*(10**(self.Dexp))/R**2)
+
+        #print(2*tf.reduce_sum(tf.math.special.bessel_j0(a_n * r / R)/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**(self.Dexp))*t/R**2), axis=2))
+
+
+        #(tf.math.special.bessel_j1(a_n)*a_n)
+        #tf.multiply(a_n,x[:,0:1]))
+        #tf.exp(tf.multiply(a_n**2,-1*x[:,1:2])*(10**(self.Dexp))/R**2)
+        #tf.math.special.bessel_j0(tf.multiply(a_n,x[:,0:1]))
+
+
+
+
+
+
+
+
+        #return (1-2*tf.reduce_sum(tf.divide(tf.math.special.bessel_j0( tf.reduce_sum(tf.multiply(a_n,x[:,0:1])))*tf.exp( tf.reduce_sum(tf.multiply(a_n**2,-1*x[:,1:2]))*(10**(self.Dexp))/R**2), (tf.math.special.bessel_j1(a_n)*a_n))))
+        return 1-2*tf.reduce_sum( tf.math.special.bessel_j0(tf.multiply(a_n,x[:,0:1]))* tf.exp(tf.multiply(a_n**2,-1*x[:,1:2])*(self.diffusionModel())/R**2)/tf.math.special.bessel_j0(tf.multiply(a_n,x[:,0:1])))
+
+###############################################################################
+#data for models
 
     def concentrationSln2(self,x):
         r = x[:,0:1]
@@ -171,10 +221,10 @@ class CO2Sim():
         for a_n in self.a_n_list:
             if i == 0:
                 an1 = a_n
-                sum = 2*tf.math.special.bessel_j0(a_n * x[:,0:1] / R)/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**self.Dexp)*x[:,1:2]*self.TimeScale/R**2)
+                sum = 2*tf.math.special.bessel_j0(a_n * x[:,0:1] )/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**self.Dexp)*x[:,1:2]*self.TimeScale/R**2)
                 i = i + 1
             else:
-                sum = sum + 2*tf.math.special.bessel_j0(a_n * x[:,0:1] / R)/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**self.Dexp)*x[:,1:2]*self.TimeScale/R**2)
+                sum = sum + 2*tf.math.special.bessel_j0(a_n * x[:,0:1])/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**self.Dexp)*x[:,1:2]*self.TimeScale/R**2)
 
         #bessel_result = tf.math.special.bessel_j0(a_n * r / R)/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**(self.Dexp))*t/R**2)
         #result_sum = 1-2*tf.reduce_sum(tf.math.special.bessel_j0(a_n * r / R)/(tf.math.special.bessel_j1(a_n)*a_n)*tf.exp(-a_n**2*(10**(self.Dexp))*t/R**2), axis=2)
@@ -187,6 +237,53 @@ class CO2Sim():
     def concentrationToViscosity(self,concentration, zeroShear):
         return concentration + zeroShear
 
+    #has limiation for D, can go between -10 and -8
+    def diffusionModel(self):
+        #Variation = tf.math.tanh(self.Dexp)-9
+        return 10**self.Dexp
+        #return 10**(tf.math.tanh(self.Dexp)-9)
+
+    #we can scale the output with the experimental torque scale, which could be large, and relatively change it with tau shift factor
+    def tauLimiter(self,tau):
+        return (tf.math.abs(tf.math.tanh(tau))/2+1)*self.torqueScale
+
+
+
+    #returns viscosity given cross parameter models and shear rate. Done for 0 Bar. Takes in shift factor too
+    def crossModel(self,x, crossParams, shiftFactor):
+        k,n,viscoZero, viscoInf = crossParams
+        differenceInVisc = viscoZero - viscoInf
+        shearRate = x[:,0:1]*self.R*self.angularVelocity/self.H*shiftFactor
+        return (differenceInVisc/(1+(k*shearRate)**n)+viscoZero)/shiftFactor
+
+
+    #linear test function of eta. See if we can guess b and m ?
+    #proven we can do this.
+    def etaFunction(self,x):
+
+        return x[:,1:2]*self.m+self.b
+
+
+    def etaFunction2(self,x):
+        MaxSolubility = self.solubilityModel.ev(self.Temperature ,self.Pressure)
+        #given nondimensionalConcentration
+        concentration = self.concentrationSln(x)
+        shiftFactorT = self.SolubilityViscModel.runWLFModelT(x, self.Temperature)
+        shiftFactorS = self.SolubilityViscModel.runWLFModelS(x, MaxSolubility*concentration)
+        #give shear rate, get viscosity
+        ViscosityNaught = self.SolubilityViscModel.CrossModelOut(x[:,0:1]*self.R*self.angularVelocity/self.H/shiftFactorT/shiftFactorS)
+        return ViscosityNaught*shiftFactorT*shiftFactorS
+
+
+
+    #returns a solubility given a pressure and temeprature.
+    def estimateSolubility(self,  Temp, Pressure):
+        pass
+
+    #shift factor for solubility data.
+    def defImportShiftFactor(self, WLFParams, solubility):
+        C1, C2 = WLFParams
+        return 10**(-C1*(solubility-0)/(C2+solubility-0))
 
 ###############################################################################
 #defining boundaries
@@ -198,12 +295,12 @@ class CO2Sim():
     def inside(self,x, on_boundary):
         return on_boundary and np.equal(x[0], self.Rmin)
     def outside(self,x, on_boundary):
-        return on_boundary and np.equal(x[0],self.R)
+        return on_boundary and np.equal(x[0],1)
 
     def generateBC(self):
         bc = dde.icbc.DirichletBC(self.geomtime, lambda x: 0, self.inside, component=0)
         ic = dde.icbc.IC(self.geomtime, self.initialCondition,
-            lambda _, on_initial: on_initial
+            lambda _, on_initial: on_initial, component= 0
             )
         #bc2 = dde.icbc.DirichletBC(self.geomtime, lambda x: self.calcTorque, self.outside)
         bc3 = dde.icbc.NeumannBC(self.geomtime, lambda x: 0, self.inside, component=0)
@@ -212,12 +309,12 @@ class CO2Sim():
 
         observation_BC =  dde.icbc.PointSetBC(self.xData, self.torqueData)
 
-        return [bc,bc3, observation_BC]
+        return [bc, bc3, observation_BC]
 
 
     def initialCondition(self,x):
-        return 2*3.14*self.etaInit*self.angularVelocity*x[:, 0:1]**3/3/self.H
-        #newSim.runBFGS(20000,[100,1,1,100])
+        #return 2
+        return 2*3.14*self.etaInit*self.angularVelocity*(x[:, 0:1]*self.R)**3/3/self.H/self.torqueScale
 
 #newSim.plotEta()
 
@@ -269,42 +366,106 @@ class CO2Sim():
         #return (eta*dVtheta_zz)
         #return (eta*dVtheta_r_r)
 
+    def rheometerPlateCO2Diff2(self,x,y):
+        Tau =  y[:, 0:1]
+        #x1 =tf.reshape(x, [-1,-1,1])
+
+        r = x[:,0:1]
+        t = x[:, 1:2]
+
+        Tau_r =  dde.grad.jacobian(y,x,i=0)
+        #viscosityMod = self.etaFunction2(x)
+
+
+
+
+
+
+
+        #eqn = ()/self.torqueScale
+        return (Tau_r-((r)**2)*(2*3.1415*(self.etaFunction2(x))*self.angularVelocity*self.R**3)/self.H/self.torqueScale)
+
+        #return (Tau_r*self.H/-((r/self.R)**2))
+        #return (eta*dVtheta_zz)
+        #return (eta*dVtheta_r_r)
+
+
+    def logrheometerPlateCO2Diff2(self,x,y):
+        Tau =  y[:, 0:1]
+        #x1 =tf.reshape(x, [-1,-1,1])
+
+        r = x[:,0:1]
+        t = x[:, 1:2]
+
+        Tau_r =  dde.grad.jacobian(y,x,i=0)
+        #viscosityMod = self.etaFunction2(x)
+
+
+
+
+
+
+
+        #eqn = ()/self.torqueScale
+        return (tf.math.log(Tau_r)-tf.math.log(((r)**2)*(2*3.1415*(self.etaFunction2(x))*self.angularVelocity*self.R**3)/self.H/self.torqueScale))
+
+        #return (Tau_r*self.H/-((r/self.R)**2))
+        #return (eta*dVtheta_zz)
+        #return (eta*dVtheta_r_r)
+
     def transformFxn(self, x, y):
         viscosityMod = self.etaFunction2(x)
         res = self.H/(2*3.1415*(viscosityMod)*self.angularVelocity*self.R**3)
         return ( y/res)
 
+    def transformFxn2(self, x, y):
+        return ( y*self.torqueScale)
 
 
 ###############################################################################
-#def model running and saving models
+#def model running and saving modelsf
 
 
     #BFGS, weights should be given in array format
-    def runBFGS(self, iterations, weights):
+    def runBFGS(self, iterations, weights, loadModel):
         self.model.compile("L-BFGS",loss_weights=weights,external_trainable_variables=self.external_trainable_variables)
         #model.train_step.optimizer_kwargs = {'options': {'maxfun': 1e5, 'ftol': 1e-20, 'gtol': 1e-20, 'eps': 1e-20, 'iprint': -1, 'maxiter': 1e5}}
         dde.optimizers.config.set_LBFGS_options(
-        maxcor=100,
+        maxcor=50,
         ftol=1.0e-50,
         gtol=1.0e-35,
-        maxiter=60000,
+        maxiter=iterations,
         maxfun=50000,
         maxls=50,
         )
+        if loadModel:
+            self.model.restore(self.modelName)
+
+
 
         self.losshistory, self.train_state = self.model.train(iterations=iterations,callbacks = [self.variable])
 
     #BFGS, weights should be given in array format, learning rate given as float
-    def runAdam(self, iterations, weights, learningRate):
+    def runAdam(self, iterations, weights, learningRate, loadModel):
 
         self.model.compile(
         "adam", lr=learningRate,loss_weights=weights,external_trainable_variables=self.external_trainable_variables
         )
+
+        if loadModel:
+            self.model.restore(self.modelName)
+
         self.losshistory, self.train_state = self.model.train(iterations=iterations,callbacks = [self.variable])
 
     def savePlot(self):
         dde.saveplot(self.losshistory, self.train_state, issave=True, isplot=True)
+
+    def saveModel(self,name):
+        self.model.save("./models/"+name)
+
+
+    def loadModel(self,modelName):
+        self.modelName = modelName
 
 
 ###############################################################################
@@ -324,59 +485,3 @@ class CO2Sim():
     def print(self):
         tf.print(self.eta, output_stream=sys.stdout)
         print("HELLO",self.eta)
-###############################################################################
-#data for models
-
-    #returns viscosity given cross parameter models and shear rate. Done for 0 Bar. Takes in shift factor too
-    def crossModel(self,x, crossParams, shiftFactor):
-        k,n,viscoZero, viscoInf = crossParams
-        differenceInVisc = viscoZero - viscoInf
-        shearRate = x[:,0:1]*self.angularVelocity/self.H*shiftFactor
-        return (differenceInVisc/(1+(k*shearRate)**n)+viscoZero)/shiftFactor
-
-
-    #linear test function of eta. See if we can guess b and m ?
-    #proven we can do this.
-    def etaFunction(self,x):
-
-        return x[:,1:2]*self.m+self.b
-
-
-    def etaFunction2(self,x):
-        MaxSolubility = self.solubilityModel.ev(self.Temperature ,self.Pressure)
-        #given nondimensionalConcentration
-        concentration = self.concentrationSln2(x)
-        shiftFactorT = self.SolubilityViscModel.runWLFModelT(x, self.Temperature)
-        shiftFactorS = self.SolubilityViscModel.runWLFModelS(x, concentration)
-        #give shear rate, get viscosity
-        ViscosityNaught = self.SolubilityViscModel.CrossModelOut(x[:,0:1]*self.angularVelocity/self.H)
-        return ViscosityNaught*shiftFactorT*shiftFactorS
-
-    #import data from excel. Returns a model that takes in pressure and temperature. This will be run once to get max solubility for a run.
-    def importSolubilityModel(self, locationOfData):
-        DataForPS = pd.read_excel(locationOfData, header = [0])
-        x= DataForPS["Pressure (MPa)"]
-        y=DataForPS["Temperature (K)"]
-        z=DataForPS["Solubility"]
-        Model = SmoothBivariateSpline(x,y,z,kx=2,ky=2, s = 0.0001,eps=0.02)
-        return Model
-
-    #returns a solubility given a pressure and temeprature.
-    def estimateSolubility(self,  Temp, Pressure):
-        pass
-
-    #shift factor for solubility data.
-    def defImportShiftFactor(self, WLFParams, solubility):
-        C1, C2 = WLFParams
-        return 10**(-C1*(solubility-0)/(C2+solubility-0))
-
-
-
-
-#sixTBar.plotCompareData2("loc")
-
-newSim = CO2Sim("file")
-
-newSim.runAdam(8000,[1,1,1,100], 0.005)
-newSim.runBFGS(10000,[1,1,1,100])
-newSim.savePlot()
--- CO2Visual.py
+++ CO2Visual.py
@@ -1,31 +1,113 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import csv
+import pandas as pd
+import glob
+import os
+from io import StringIO
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+from scipy.signal import savgol_filter
+from scipy.optimize import minimize
+from scipy.interpolate import SmoothBivariateSpline
+from scipy import special as spe
+import seaborn as sns
+import math
+#import ipywidgets as widgets
+#from IPython.display import display
+from CO2Fxn import fitCrossAndWLF
 
-import ipywidgets as widgets
-from IPython.display import display
+class dataAnalysis:
 
-# Load your data
-train_data = np.loadtxt('train.dat')
-test_data = np.loadtxt('test.dat')
+    def __init__(self):
+        self.fileLocations = []
+        self.LiteratureDataLoc = "./TrainingData/MiscFiles/DataFromLiterature.xlsx"
+        self.LiteratureData = ""
+        self.DConstants = []
+        self.collectedData = ""
+        self.SolubilityViscModel = fitCrossAndWLF("./CombinedDataPS_p2.xlsx")
 
-def plot_3d(elev=0., azim=0.):
-    fig = plt.figure(figsize=(10, 8))
-    # Subplot for train data
-    ax1 = fig.add_subplot(121, projection='3d')
-    ax1.scatter(train_data[:, 0], train_data[:, 1], train_data[:, 2])
-    ax1.view_init(elev=elev, azim=azim)
-    ax1.set_title('Train Data')
-    # Subplot for test data
-    ax2 = fig.add_subplot(122, projection='3d')
-    ax2.scatter(test_data[:, 0], test_data[:, 1], test_data[:, 2])
-    ax2.view_init(elev=elev, azim=azim)
-    ax2.set_title('Test Data')
-    plt.show()
 
-# Create widgets
-elev_slider = widgets.FloatSlider(min=-90., max=90., step=5., value=0.)
-azim_slider = widgets.FloatSlider(min=-180., max=180., step=5., value=0.)
 
-# Create interactive plot
-widgets.interact(plot_3d, elev=elev_slider, azim=azim_slider)
+    def readFiles(self, folderLocation):
+        self.fileLocations = os.listdir(folderLocation)
+        print(self.fileLocations)
+        for file in self.fileLocations:
+            if ".swp" in file:
+                continue
+            #print(file)
+            substrings = file.split("-")
+            Pressure = substrings[0]
+            Temperature = substrings[1].replace("variables1.dat","")
+            dataf1 = pd.read_table(folderLocation+file, sep = " ", names = ["Epoch","Value"])
+            dataf1["Value1"] =  dataf1["Value"].str.replace("[","").str.replace("]","").astype(float)
+            #dataf1["Value1"] =  dataf1["Value"].astype(float)
+
+            dataf1["DConstant"] = 10**(np.tanh(dataf1["Value1"])-9)
+            #print(dataf1)
+            dconst = dataf1.iloc[-1]["DConstant"]
+            tupleOfData = (Pressure,Temperature,dconst)
+            self.DConstants.append(tupleOfData)
+            #print(dconst)
+        self.collectedData = pd.DataFrame(data = self.DConstants, columns = ["Pressure", "Temperature", "D"])
+        self.collectedData["Temperature (K)"] = self.collectedData["Temperature"].astype(float)+273.13
+        self.collectedData["Average pressure (MPa)"] = self.collectedData["Pressure"].astype(float)/10
+        #self.collectedData["Average solubility (g-gas kg-polym)"] = self.SolubilityViscModel.evaluateSolubility(100,20.6)
+        self.collectedData["From"] = "ML Calculations"
+        self.collectedData["Average solubility (g-gas kg-polym)"] = self.SolubilityViscModel.evaluateSolubility(self.collectedData["Temperature"].astype(float),self.collectedData["Pressure"].astype(float))
+        #self.collectedData["Pressure (MPa)"] = self.collectedData["Temperature"]
+
+
+        print(self.collectedData)
+
+    def readLitData(self):
+        self.LiteratureData = pd.read_excel(self.LiteratureDataLoc)
+        #print(self.LiteratureData )
+
+    def plotAllData(self):
+        totalData = pd.concat([self.collectedData,self.LiteratureData])
+        print(totalData)
+        fig, ax = plt.subplots(figsize=(14, 10))
+        a1 = sns.scatterplot(
+            data=totalData,  style = "From",  x = "Temperature (K)",
+            hue="Average solubility (g-gas kg-polym)", y="D", palette = "flare", s = 100, edgecolor = "none", ax = ax
+            );
+        plt.yscale("log")
+        plt.show()
+
+
+
+    def someRandomFxn(self):
+
+        # Load your data
+        train_data = np.loadtxt('train.dat')
+        test_data = np.loadtxt('test.dat')
+
+        def plot_3d(elev=0., azim=0.):
+            fig = plt.figure(figsize=(10, 8))
+            # Subplot for train data
+            ax1 = fig.add_subplot(121, projection='3d')
+            ax1.scatter(train_data[:, 0], train_data[:, 1], train_data[:, 2])
+            ax1.view_init(elev=elev, azim=azim)
+            ax1.set_title('Train Data')
+            # Subplot for test data
+            ax2 = fig.add_subplot(122, projection='3d')
+            ax2.scatter(test_data[:, 0], test_data[:, 1], test_data[:, 2])
+            ax2.view_init(elev=elev, azim=azim)
+            ax2.set_title('Test Data')
+            plt.show()
+
+        # Create widgets
+        elev_slider = widgets.FloatSlider(min=-90., max=90., step=5., value=0.)
+        azim_slider = widgets.FloatSlider(min=-180., max=180., step=5., value=0.)
+
+        # Create interactive plot
+        widgets.interact(plot_3d, elev=elev_slider, azim=azim_slider)
+
+data1 = dataAnalysis()
+
+data1.readFiles("./variableTrained/")
+data1.readLitData()
+data1.plotAllData()
